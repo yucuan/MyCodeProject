4c796c79f6e5d0336774e8773fb9373722b0e947
yucuan <103493216+yucuan@users.noreply.github.com>
2022-04-12 16:44:01 +0800

    Add files via upload

---
+++ StepUp.py
@@ -0,0 +1,54 @@
+"""
+File: StepUp.py
+Name: TODO:
+------------------------
+This file shows Karel picking up 
+the beeper at Street 1 Avenue 2,
+putting it onto Street 2 Avenue 4.
+Karel will be facing East at Street
+2 Avenue 5 at the end of this program.
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    move()
+    pick_beeper()
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    put_99()
+    move()
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def put_99():
+    for i in range(99):
+        put_beeper()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####### DO NOT EDIT CODE BELOW THIS LINE ########
+
+if __name__ == '__main__':
+    execute_karel_task(main)
